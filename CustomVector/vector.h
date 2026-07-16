4c7b0b3fe24102f97821226102d8af444506f132
innacode413 <innacode413@github.com>
2026-07-16 17:49:54 +0300

    Add copy ctor, copy assignment, operator[], ==, != to vector

--- CustomVector/vector.h
+++ CustomVector/vector.h
@@ -3,12 +3,21 @@
 class vector {
 public:
     vector(int sizeOffArray);
+    vector(const vector& other);
     ~vector();
+
+    vector& operator=(const vector& other);
 
     void resize(int newSize);
     void set(int index, int newValue);
     int get(int index) const;
     int getSize() const;
+
+    int& operator[](int index);
+    const int& operator[](int index) const;
+
+    bool operator==(const vector& other) const;
+    bool operator!=(const vector& other) const;
 
 private:
     int size;
